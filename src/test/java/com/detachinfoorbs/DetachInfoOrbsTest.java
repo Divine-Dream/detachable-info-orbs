94f30b6960b6be877577fbfd5353abc8d9d1c8b2
Brock Thomas <brockthomas18@gmail.com>
2025-07-04 17:27:28 -0600

    Refactor "example" code

--- src/test/java/com/detachinfoorbs/DetachInfoOrbsTest.java
+++ src/test/java/com/detachinfoorbs/DetachInfoOrbsTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.detachinfoorbs;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class DetachInfoOrbsTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(DetachInfoOrbsPlugin.class);
 		RuneLite.main(args);
 	}
 }
